89c8b6550e16365df7af874129bf67766653dc86
Rohit Sahu <rohit2sahu@Rohits-MacBook-Air.local>
2026-04-20 14:24:04 +0530

    FinacPlus Assignment

--- src/tests/Api_Task_FinacplusAssignment.spec.ts
+++ src/tests/Api_Task_FinacplusAssignment.spec.ts
@@ -1,6 +1,14 @@
 import { expect,test } from "playwright/test";
 import { CRUD_UserDetails } from "../main/Utilities/API_Service/api_service_layer";
 
+
+/*
+This test covers an end-to-end API automation flow starting with the creation of a new user via a POST request.
+ The response is validated by checking the HTTP status code and extracting the generated user ID. Using this user ID,
+  a GET request is made to retrieve the user details and validate the correctness of the stored data. Further,
+  the test performs an update operation on the username using a PUT request and verifies that the updated details are reflected correctly for the same user ID, ensuring consistency across all API operations.
+*/
+
 test('FinacPlus API Assignment',async({page,request})=>{
     const obj=  new CRUD_UserDetails(page);
     await obj.createNewUserDetails(request);
--- src/tests/UI_Task_FinacPlusAssignment.spec.ts
+++ src/tests/UI_Task_FinacPlusAssignment.spec.ts
@@ -5,6 +5,12 @@ import { userData } from '../main/Utilities/DataSeviceLayer/userData';
 import {test,expect} from "../main/fixture/bookApplicationFixture"
 import { CRUD_UserDetails } from '../main/Utilities/API_Service/api_service_layer';
 
+/*
+In this test i had implemented end-to-end UI automation flow starting from the home page navigation to the Book Store application.
+ The flow includes user login, navigating to the book list, selecting the specified book, and extracting details such as title, author, and publisher.
+  The extracted data is then written and appended to an output.txt file for persistence and validation.
+*/
+
 test("FinacPlus UI Assignment",async({bookHomeApplication,bookLoginServices})=>{
 
     await bookLoginServices.searchForBooks();
